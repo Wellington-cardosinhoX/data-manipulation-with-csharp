f0680ad2aca1fb4ea1e56559492479908d55fd9c
dpcosta <dpcosta@gmail.com>
2025-08-07 18:23:34 -0300

    1.3 - Percorrendo coleções com a interface IEnumerable e foreach

--- 01-IntroAColecoes/Program.cs
+++ 01-IntroAColecoes/Program.cs
@@ -33,6 +33,46 @@ var playlist2 = new List<Musica>
     new Musica { Titulo = "Stairway to Heaven", Artista = "Led Zeppelin" }
 };
 
+
+// vamos imprimir a playlist no terminal
+foreach (var musica in playlist)
+{
+    Console.WriteLine($"Título: {musica.Titulo}, Artista: {musica.Artista}");
+}
+
+/*
+    como o foreach funciona por baixo dos panos? 
+*/
+
+var enumerador = playlist.GetEnumerator();
+while (enumerador.MoveNext())
+{
+    //a cada chamada de MoveNext(), o cursor avança para o próximo item. Current retorna o item atual.
+    var musica = enumerador.Current;
+    Console.WriteLine($"Título: {musica.Titulo}, Artista: {musica.Artista}");
+}
+
+// esse código é muito verboso, então o C# nos permite usar o foreach para simplificar :-)
+
+/*
+    foreach só pode ser usado em coleções que implementam IEnumerable:
+    https://learn.microsoft.com/pt-br/dotnet/csharp/language-reference/statements/iteration-statements#the-foreach-statement
+
+    IEnumerable é uma interface que representa uma coleção que pode ser enumerada ("percorrida")
+    https://learn.microsoft.com/pt-br/dotnet/api/system.collections.generic.ienumerable-1
+
+     se eu fizer:
+     foreach (var item in musica1)
+     {
+         Console.WriteLine(item);
+     }
+
+    não vai funcionar, porque Musica não implementa IEnumerable.
+ 
+*/
+
+
+
 public class Produto
 {
     public required string Id { get; set; }
